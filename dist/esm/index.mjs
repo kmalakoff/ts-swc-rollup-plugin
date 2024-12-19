26f270d5943114bb8518af236bb041a2928c704d
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-19 07:10:37 -0800

    Upgrade modules

--- dist/esm/index.mjs
+++ dist/esm/index.mjs
@@ -1,7 +1,8 @@
 import getTS from 'get-tsconfig-compat';
-import process from 'process';
 import { createMatcher, resolveFileSync, transformSync } from 'ts-swc-transform';
 import { typeFileRegEx } from './constants.mjs';
+// @ts-ignore
+import process from './lib/process.cjs';
 export default function swc(options = {}) {
     const tsconfig = typeof options.tsconfig === 'object' ? options.tsconfig : getTS.getTsconfig(options.cwd || process.cwd(), options.tsconfig || 'tsconfig.json');
     if (!tsconfig) throw new Error(`tsconfig not found in: ${options.cwd || process.cwd()} named: ${options.tsconfig || 'tsconfig.json'}`);
--- test/data/src/index.ts
+++ test/data/src/index.ts
@@ -1,39 +1,35 @@
 import getTS from 'get-tsconfig-compat';
-import { createMatcher, resolveFileSync, transformSync } from 'ts-swc-transform';
-import { typeFileRegEx } from './constants.cjs'
-
-// @ts-ignore
-import process from './process.cjs';
-
 import type { TsConfigResult } from 'get-tsconfig-compat';
-import type { TransformResult } from 'rollup';
+import type { Plugin } from 'rollup';
+import { createMatcher, resolveFileSync, transformSync } from 'ts-swc-transform';
+import { typeFileRegEx } from './constants.js';
+// @ts-ignore
+import process from './lib/process.cjs';
 
 export type { TsConfigResult } from 'get-tsconfig-compat';
-export type { TransformResult } from 'rollup';
 export interface SWCOptions {
   cwd?: string;
   tsconfig?: TsConfigResult | string;
 }
 
-export default function swc(options: SWCOptions = {}) {
+export default function swc(options: SWCOptions = {}): Plugin<SWCOptions> {
   const tsconfig = typeof options.tsconfig === 'object' ? options.tsconfig : getTS.getTsconfig(options.cwd || process.cwd(), options.tsconfig || 'tsconfig.json');
   if (!tsconfig) throw new Error(`tsconfig not found in: ${options.cwd || process.cwd()} named: ${options.tsconfig || 'tsconfig.json'}`);
 
   const matcher = createMatcher(tsconfig);
   return {
     name: 'ts-swc',
-    transform(code: string, id: string): TransformResult {
-      console.log(code, id);
+    transform(code: string, id: string) {
       return transformSync(code, id, tsconfig);
     },
 
-    resolveId(specifier: string, parentPath?: string) { 
+    resolveId(specifier: string, parentPath?: string) {
       const context = { parentPath };
       const filePath = resolveFileSync(specifier, context);
-      if (!filePath) return null
+      if (!filePath) return null;
       if (!matcher(filePath)) return null;
       if (typeFileRegEx.test(filePath)) return null;
       return filePath;
-    }
+    },
   };
 }
