1d1981fc821be4056b69f492ca3ed51fc84c0008
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-18 12:59:48 -0800

    Locked versions

--- test/lib/compat.ts
+++ test/lib/compat.ts
@@ -1,11 +1,5 @@
 import os from 'os';
 
-var hasHomedir = typeof os.homedir === 'function';
-
 export function homedir(): string {
-  if (hasHomedir) {
-    return os.homedir();
-  }
-  var home = require('homedir-polyfill');
-  return home();
+  return typeof os.homedir === 'function' ? os.homedir() : require('homedir-polyfill')();
 }
