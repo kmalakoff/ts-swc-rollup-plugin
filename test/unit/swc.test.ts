7a8938b765132f5b9ea33951d77ec3d2e6e41d18
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-12 09:29:01 -0700

    Throw when tsconfig is missing

--- test/unit/swc.test.ts
+++ test/unit/swc.test.ts
@@ -3,6 +3,8 @@ import fs from 'fs';
 import path from 'path';
 import url from 'url';
 import getTS from 'get-tsconfig-compat';
+import home from 'os-homedir';
+import type { SourceDescription } from 'rollup';
 
 // @ts-ignore
 import { swc } from 'ts-swc-rollup-plugin';
@@ -16,7 +18,7 @@ describe('swc', () => {
   it('no overrides', () => {
     const plugin = swc({ cwd });
     assert.equal(typeof plugin.transform, 'function');
-    const result = plugin.transform(sourceContent, sourcePath);
+    const result = plugin.transform(sourceContent, sourcePath) as SourceDescription;
     assert.equal(typeof result.code, 'string');
     assert.equal(typeof result.map, 'string');
     assert.ok(result.code.indexOf('export default function swc') >= 0);
@@ -26,7 +28,7 @@ describe('swc', () => {
   it('option tsconfig tsconfigES5.json', () => {
     const plugin = swc({ cwd, tsconfig: 'tsconfigES5.json' });
     assert.equal(typeof plugin.transform, 'function');
-    const result = plugin.transform(sourceContent, sourcePath);
+    const result = plugin.transform(sourceContent, sourcePath) as SourceDescription;
     assert.equal(typeof result.code, 'string');
     assert.equal(typeof result.map, 'string');
     assert.ok(result.code.indexOf('export default function swc') >= 0);
@@ -38,10 +40,19 @@ describe('swc', () => {
     tsconfig.config.compilerOptions = { ...tsconfig.config.compilerOptions, target: 'ES5', module: 'commonjs' };
     const plugin = swc({ cwd, tsconfig });
     assert.equal(typeof plugin.transform, 'function');
-    const result = plugin.transform(sourceContent, sourcePath);
+    const result = plugin.transform(sourceContent, sourcePath) as SourceDescription;
     assert.equal(typeof result.code, 'string');
     assert.equal(typeof result.map, 'string');
     assert.ok(result.code.indexOf('Object.defineProperty(exports, "default"') >= 0);
     assert.ok(result.code.indexOf('var tsconfig =') >= 0);
+  });
+
+  it('error', () => {
+    try {
+      swc({ cwd: home() });
+      assert.ok(false, 'this should fail');
+    } catch (err) {
+      assert.ok(err);
+    }
   });
 });
